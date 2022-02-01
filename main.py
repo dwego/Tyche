dc0bdba5e25b24de81e25e5bc1d77ade0a74cc53
dwego <dweg0epic@outlook.com>
2022-02-01 18:50:50 -0300

    Sintese de fala

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from unittest import result
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import json
+import pyttsx3
+
+
+#Sintese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-1].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
@@ -10,12 +24,16 @@ stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, fram
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(4000, exception_on_overflow=False)
+
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text =  result['text']
+        
+        print(text)
+        speak(text)
